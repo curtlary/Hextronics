a8640bd611304c81b318d7f24488aa2b71bd5a96
curtlary <49045750+curtlary@users.noreply.github.com>
2020-08-30 20:50:30 -0400

    Add files via upload

---
+++ camera code python.py
@@ -0,0 +1,12 @@
+from picamera import PiCamera
+import time
+
+camera = PiCamera()
+
+camera.resolution = (640,480);
+camera.vflip = True
+#camera.start_preview()
+time.sleep(2)
+
+camera.capture("far10.jpg")
+
