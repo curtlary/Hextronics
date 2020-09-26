ea43620e6f5a17f41d50b0414d4ec4fad2b2399f
Davi <davi.nakajima.an@gmail.com>
2020-09-26 15:32:11 -0400

    capture and show plat

--- img_capture/capture_and_compare.py
+++ img_capture/capture_and_compare.py
@@ -3,6 +3,8 @@ import datetime
 import sys
 import time
 import subprocess
+from cv.feature_extraction import find_homography, apply_homography, get_platform_mask
+import matplotlib.pyplot as plt
 
 #read the absolute path
 script_dir = os.path.dirname(__file__)
@@ -18,3 +20,13 @@ rel_path = currentdate +".jpg"
 
 #join the absolute path and created file name
 abs_file_path = os.path.join(script_dir, rel_path)
+
+#get captured image again:
+img = plt.imread(abs_file_path)
+plat = get_platform_mask(img)
+
+plt.imshow(img)
+plt.show()
+plt.imsho(plat)
+plt.show()
+
