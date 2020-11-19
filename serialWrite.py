5b1da65537c9b5d3ac778d9f10b5f9d939f2583e
Davi <davi.nakajima.an@gmail.com>
2020-11-19 14:17:58 -0500

    adding support for python image capture

--- serialWrite.py
+++ serialWrite.py
@@ -7,6 +7,24 @@ from datetime import datetime
 
 os.system('sudo stty -F /dev/ttyACM0 -hupcl')
 
+def write(command):
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1,dsrdtr=False)
+    ser.flush()
+
+    starttime = datetime.now()
+    output = str(command) + " \n"
+    ser.write(output.encode('utf-8'))
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+            if(line == "Arrived!"):
+                break
+        timedelta = datetime.now() - starttime
+        if timedelta.total_seconds() >= 10:
+            break
+
+
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1,dsrdtr=False)
     ser.flush()
