c393ca8b32d742e747bede70254d54688315ae6c
Davi <davi.nakajima.an@gmail.com>
2020-12-08 16:16:28 -0600

    demo

--- workflow/oop_workflow.py
+++ workflow/oop_workflow.py
@@ -7,7 +7,16 @@ import time
 from readGPIO import readGPIO
 
 controller = ActuatorController()
-locator = DroneLocator()
+locator = DroneLocator(
+    linreg_path="cv/offset_model.pkl",
+    canny_thresholds=(200, 200),
+    show_circles=False,
+    rough_radius_range=(20, 60),
+    fine_radius_range=(30, 60),
+    qr2but_range=(150, 600),
+    button_color_hsv_low=(25, 50, 100),
+    button_color_hsv_high=(40, 255, 255),
+)
 video_maker = VideoMaker(run_name="test")
 planner = PathPlanner(locator=locator, controller=controller, video_maker=video_maker)
 
@@ -19,6 +28,7 @@ while True:
         print("INITIATED")
         controller.open_roof()
         time.sleep(20)
+        planner.wait()
         planner.search_main()
         time.sleep(240)
         controller.close_roof()
