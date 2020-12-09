48392e5b203a85c9dee9a66105b7acc4c4bf2a0d
Davi <davi.nakajima.an@gmail.com>
2020-12-09 01:10:18 -0500

    oi

--- monitor.py
+++ monitor.py
@@ -2,6 +2,7 @@
 import numpy as np
 from cv.locator import DroneLocator
 import cv2
+import time
 
 
 cap = cv2.VideoCapture(2)
@@ -15,26 +16,43 @@ locator = DroneLocator(
     button_color_hsv_low=(25, 50, 100),
     button_color_hsv_high=(40, 255, 255),
 )
+prev_qr = False
+count = 0
 while(True):
     ret, frame = cap.read()
     qr, x, y, angle = locator(frame)
+
     if qr:
-        if angle == 404:
-            cv2.circle(frame, [x, y], (255, 0, 0), 10)
-            cv2.circle(frame, [x, y], (255, 0, 0), 10)
-            cv2.putText(
-                frame,
-                str(angle),
-                (x + 10, y + 10),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (128, 255, 255),
-                2,
-            )
-    cv2.imshow('frame', frame)
+
+        cv2.circle(frame, (int(x), int(y)), 47, (255, 0, 0), 10)
+        cv2.putText(
+            frame,
+            f"Offset{(x, y)}, Angle: {angle}",
+            (0, 60),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (128, 255, 255),
+            2,
+        )
+        #print("oi")
+    if count > 2:
+        cv2.putText(
+            frame,
+            f"Found: {qr}",
+            (0, 20),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (128, 255, 255),
+            2,
+        )
+        # print(qr)
+        count = 0
+    count += 1
+    prev_qr = qr
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+  
 
 cap.release()
 cv2.destroyAllWindows()
